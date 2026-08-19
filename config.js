2a9702dd65cfb319c1ae4996f3bf4fd6b4ee7e54
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-08-19 09:54:16 +0300

    Add storageBucket to VIBES_CONFIG in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.VIBES_CONFIG = Object.freeze({
   supabaseUrl: 'https://ajmymjzjmkxupbtcdwbc.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqbXltanpqbWt4dXBidGNkd2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMDM5ODgsImV4cCI6MjEwMjY3OTk4OH0.UdwSw1hkGIPHr3xzEhGHhDTrmfPwO1DqJagBrv0Wx7A'
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqbXltanpqbWt4dXBidGNkd2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMDM5ODgsImV4cCI6MjEwMjY3OTk4OH0.UdwSw1hkGIPHr3xzEhGHhDTrmfPwO1DqJagBrv0Wx7A',
+  storageBucket: 'products'
 });
